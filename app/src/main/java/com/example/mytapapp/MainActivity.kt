0c642dd86992e23f13c87029be67103e30eca3eb
flash9600 <trefilovdimarus@gmail.com>
2023-09-13 20:04:44 +0300

    try

--- app/src/main/java/com/example/mytapapp/MainActivity.kt
+++ app/src/main/java/com/example/mytapapp/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.mytapapp
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -30,10 +24,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         val callIntent = Intent(Intent.ACTION_VIEW, callUri)
         if (intent.resolveActivity(packageManager) != null) {
             startActivity(callIntent)
-        }
-        val geetContent = registerForActivityResult(GetContent()) { uri: Uri? ->
-            // Handle the returned Uri
-        }
         }
     }
 
