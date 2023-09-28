e9325fffdaccadd041e714bab4e3979b9cc2b693
flash9600 <trefilovdimarus@gmail.com>
2023-09-28 17:53:16 +0300

    toast

--- app/src/main/java/com/example/mytapapp/MainActivity.kt
+++ app/src/main/java/com/example/mytapapp/MainActivity.kt
@@ -1,36 +1,19 @@
 package com.example.mytapapp
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var callButton: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        callButton = findViewById(R.id.image_button)
-        callButton.setOnClickListener {
-                openTakeImageIntent()
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
         }
 
-    }
-
-    private fun openTakeImageIntent() {
-        val callUri = Uri.parse("https://developer.alexanderklimov.ru/android/theory/intent.php")
-        val callIntent = Intent(Intent.ACTION_VIEW, callUri)
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(callIntent)
-        }
     }
 
 }
