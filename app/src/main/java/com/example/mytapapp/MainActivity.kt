f7bc2796f912a3029c6ab86a815ba508d03b3e7f
flash9600 <trefilovdimarus@gmail.com>
2023-09-28 18:16:43 +0300

    dice

--- app/src/main/java/com/example/mytapapp/MainActivity.kt
+++ app/src/main/java/com/example/mytapapp/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.mytapapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
-        }
+        rollButton.setOnClickListener {rollDice()}
 
+
+    }
+
+    private fun rollDice()  {
+        val diceValue =  Dice(6).roll()
+        findViewById<TextView>(R.id.textView).text = diceValue.toString()
     }
 
 }
