668bd05d6a864b76dfb18cef58a1d8c48f928eb0
flash9600 <trefilovdimarus@gmail.com>
2023-11-22 19:11:20 +0400

    implemented recyclerView list

--- app/src/main/java/com/example/mytapapp/MainActivity.kt
+++ app/src/main/java/com/example/mytapapp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.mytapapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mytapapp.R.layout
+import com.example.mytapapp.adapter.ItemAdapter
+import com.example.mytapapp.data.Datasourse
 import com.example.mytapapp.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -10,36 +13,12 @@ class MainActivity : AppCompatActivity(layout.activity_main) {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.calculateButton.setOnClickListener { calculateTip() }
+        setContentView(R.layout.activity_main)
+        val myDataset = Datasourse().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
     }
 
-
-    private fun calculateTip() {
-        val stringInTextField = binding.coastOfService.text.toString()
-        val cost = stringInTextField.toDoubleOrNull()
-        if (cost == null) {
-            binding.tipResult.text = ""
-            return
-        }
-        val tipPercentage =
-            when (binding.tipOptions.checkedRadioButtonId) {
-                binding.optionTwentyPercent.id -> 0.20
-                binding.optionEighteenPercent.id -> 0.18
-                else -> 0.15
-            }
-
-
-        val formattedTip = {
-            var tip = tipPercentage * cost
-            if (binding.roundUpSwitch.isChecked) {
-                tip = kotlin.math.ceil(tip)
-            }
-
-            NumberFormat.getCurrencyInstance().format(tip)
-        }
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip())
-    }
 
 }
---
+++ app/src/main/java/com/example/mytapapp/model/Affirmation.kt
@@ -0,0 +1,4 @@
+package com.example.mytapapp.model
+
+data class Affirmation(val stringResourceId: Int) {
+}
